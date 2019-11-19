d294aed0d9b4f49eb5b4312690129567d2eff41a
wuyang <wuyang@cictec.cn>
2019-11-19 18:43:05 +0800

    增加配置

--- src/config/settings.js
+++ src/config/settings.js
@@ -14,29 +14,29 @@ export const PLATFORM_PREFIX_NAME = 'NOCTURNE_UI'
   * API接口默认参数配置
   */
 let id = Math.random().toString(36).substr(3, 30)
-// let ip = window.location.host
-// let baseUrl = ''
-// if (ip.indexOf('192.168') !== -1) {
-//   baseUrl = 'http://192.168.0.48:12345'
-// } else {
-//   baseUrl = 'http://172.16.100.160:8081'
-// }
-export const WSAPI = `ws://111.62.52.35:8089/websocket/mesgshow/${id}`
+let ip = window.location.host
+let baseUrl = ''
+if (ip.indexOf('192.168') !== -1) {
+  baseUrl = 'http://192.168.200.26:1234'
+} else {
+  baseUrl = 'http://111.62.52.35:8089'
+}
+export const WSAPI = `ws://${baseUrl.substring(7, baseUrl.length)}/websocket/mesgshow/${id}`
 export const API_DEFAULT_CONFIG = {
   // mockBaseUrl: 'http://192.168.10.145:8082',
   // mockBaseUrl: 'http://111.62.52.35:8089',
   // mockBaseUrl: 'http://117.34.118.30:8089',
   // mockBaseUrl: 'http://61.157.184.120:8088',
   // mockBaseUrl: 'http://192.168.10.23:8081',
-  // mockBaseUrl: 'http://192.168.10.81:8081',
-  mockBaseUrl: 'http://111.62.52.35:8089', // 邢台
+  mockBaseUrl: 'http://192.168.10.81:8081',
+  // mockBaseUrl: 'http://111.62.52.35:8089', // 邢台
   // 192.168.10.33:8081
   // mockBaseUrl: 'http://192.168.10.33:8081',
   // 10.233 8089
   // prodBaseUrl: 'http://192.168.10.233:8089', // 测试
   // prodBaseUrl: 'http://117.34.118.30:8089', // 自测
   // prodBaseUrl: 'http://61.157.184.120:8088', // 宜宾
-  prodBaseUrl: 'http://111.62.52.35:8089', // 邢台
+  prodBaseUrl: baseUrl, // 邢台
   isMocked: process.env.NODE_ENV !== 'production',
   isDebug: true,
   sep: '.'
