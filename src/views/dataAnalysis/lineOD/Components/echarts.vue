f33f6724342a112172797c499f4aa198da2d2097
wuyang <wuyang@cictec.cn>
2020-01-16 09:27:37 +0800

    测试新功能

--- src/views/dataAnalysis/lineOD/Components/echarts.vue
+++ src/views/dataAnalysis/lineOD/Components/echarts.vue
@@ -5,127 +5,127 @@
 </template>
 
 <script>
-import G2 from '@antv/g2'
-import DataSet from '@antv/data-set'
-import { mapGetters } from 'vuex'
-export default {
-  props: {
-    selectData: {
-      type: Object
-    }
-  },
-  data () {
-    return {
-      echartsData: {}
-    }
-  },
-  computed: {
-    ...mapGetters(['defaultSearch'])
-  },
-  created () {
-  },
-  mounted () {
-    setTimeout(() => {
-      let lineArr = this.defaultSearch.lineLineId.split('+')
-      this._getPfODYCountListData({
-        company: this.defaultSearch.lineOrgId,
-        lineID: lineArr[0],
-        arrow: this.defaultSearch.lineType
-      })
-    }, 500)
-  },
-  watch: {
-    selectData: {
-      deep: true,
-      handler (newV) {
-        // if () {}
-        let lineArr = newV.lineLineId.split('+')
-        this._getPfODYCountListData({
-          company: newV.lineOrgId,
-          lineID: lineArr[0],
-          arrow: newV.lineType
-        })
-      }
-    }
-  },
-  methods: {
-    _getPfODYCountListData (params) {
-      this.$api['lineNet.getPfODYCountListData'](params).then(res => {
-        let nodes = []
-        let links = []
-        if (res.nodes.length > 0) {
-          res.nodes.forEach(item => {
-            nodes.push({
-              id: item.sStationIndex,
-              name: item.sStationName,
-              value: item.brushCount
-            })
-          })
-          res.edges.forEach(item => {
-            links.push({
-              source: item.sStationIndex,
-              target: item.eStationIndex
-            })
-          })
-          let ds = new DataSet()
-          let dv = ds.createView().source({ links, nodes }, {
-            type: 'graph',
-            edges: function edges (d) {
-              return d.links
-            }
-          })
-          dv.transform({
-            type: 'diagram.arc',
-            marginRatio: 0.5
-          // sortBy: 'frequency' // id, weight, frequency, {function}
-          })
-          console.log(dv)
-          let chartDom = this.$refs.chartWrapper
-          console.log(chartDom.childNodes)
-          if (chartDom.childNodes.length > 0) {
-            chartDom.removeChild(chartDom.firstChild)
-          }
-          let chart = new G2.Chart({
-            container: 'classer',
-            forceFit: true,
-            height: 570,
-            padding: ['auto', 'auto', 100, 'auto']
-          })
+// import G2 from '@antv/g2'
+// import DataSet from '@antv/data-set'
+// import { mapGetters } from 'vuex'
+// export default {
+//   props: {
+//     selectData: {
+//       type: Object
+//     }
+//   },
+//   data () {
+//     return {
+//       echartsData: {}
+//     }
+//   },
+//   computed: {
+//     ...mapGetters(['defaultSearch'])
+//   },
+//   created () {
+//   },
+//   mounted () {
+//     setTimeout(() => {
+//       let lineArr = this.defaultSearch.lineLineId.split('+')
+//       this._getPfODYCountListData({
+//         company: this.defaultSearch.lineOrgId,
+//         lineID: lineArr[0],
+//         arrow: this.defaultSearch.lineType
+//       })
+//     }, 500)
+//   },
+//   watch: {
+//     selectData: {
+//       deep: true,
+//       handler (newV) {
+//         // if () {}
+//         let lineArr = newV.lineLineId.split('+')
+//         this._getPfODYCountListData({
+//           company: newV.lineOrgId,
+//           lineID: lineArr[0],
+//           arrow: newV.lineType
+//         })
+//       }
+//     }
+//   },
+//   methods: {
+//     _getPfODYCountListData (params) {
+//       this.$api['lineNet.getPfODYCountListData'](params).then(res => {
+//         let nodes = []
+//         let links = []
+//         if (res.nodes.length > 0) {
+//           res.nodes.forEach(item => {
+//             nodes.push({
+//               id: item.sStationIndex,
+//               name: item.sStationName,
+//               value: item.brushCount
+//             })
+//           })
+//           res.edges.forEach(item => {
+//             links.push({
+//               source: item.sStationIndex,
+//               target: item.eStationIndex
+//             })
+//           })
+//           let ds = new DataSet()
+//           let dv = ds.createView().source({ links, nodes }, {
+//             type: 'graph',
+//             edges: function edges (d) {
+//               return d.links
+//             }
+//           })
+//           dv.transform({
+//             type: 'diagram.arc',
+//             marginRatio: 0.5
+//           // sortBy: 'frequency' // id, weight, frequency, {function}
+//           })
+//           console.log(dv)
+//           let chartDom = this.$refs.chartWrapper
+//           console.log(chartDom.childNodes)
+//           if (chartDom.childNodes.length > 0) {
+//             chartDom.removeChild(chartDom.firstChild)
+//           }
+//           let chart = new G2.Chart({
+//             container: 'classer',
+//             forceFit: true,
+//             height: 570,
+//             padding: ['auto', 'auto', 100, 'auto']
+//           })
 
-          chart.legend(false)
-          chart.tooltip({
-            showTitle: false
-          })
+//           chart.legend(false)
+//           chart.tooltip({
+//             showTitle: false
+//           })
 
-          let edgeView = chart.view()
-          edgeView.source(dv.edges.filter(item => item.x))
-          edgeView.axis(false)
-          edgeView.edge().position('x*y').shape('arc').color('source').opacity(0.5).tooltip('source*target')
+//           let edgeView = chart.view()
+//           edgeView.source(dv.edges.filter(item => item.x))
+//           edgeView.axis(false)
+//           edgeView.edge().position('x*y').shape('arc').color('source').opacity(0.5).tooltip('source*target')
 
-          let nodeView = chart.view()
-          nodeView.source(dv.nodes)
-          nodeView.axis(false)
-          nodeView.point().position('x*y').shape('circle').size('value').color('id').opacity(0.5).style({
-            stroke: 'grey'
-          }).label('name', { // label configuration for non-polar coord
-            offset: -10,
-            textStyle: {
-              textAlign: 'left',
-              rotate: 90
-            }
-          })
-          chart.render()
-        } else {
-          let chartDom = this.$refs.chartWrapper
-          if (chartDom.childNodes.length > 0) {
-            chartDom.removeChild(chartDom.firstChild)
-          }
-          this.$message.warning('暂无数据')
-        }
-      })
-    }
-  }
-}
+//           let nodeView = chart.view()
+//           nodeView.source(dv.nodes)
+//           nodeView.axis(false)
+//           nodeView.point().position('x*y').shape('circle').size('value').color('id').opacity(0.5).style({
+//             stroke: 'grey'
+//           }).label('name', { // label configuration for non-polar coord
+//             offset: -10,
+//             textStyle: {
+//               textAlign: 'left',
+//               rotate: 90
+//             }
+//           })
+//           chart.render()
+//         } else {
+//           let chartDom = this.$refs.chartWrapper
+//           if (chartDom.childNodes.length > 0) {
+//             chartDom.removeChild(chartDom.firstChild)
+//           }
+//           this.$message.warning('暂无数据')
+//         }
+//       })
+//     }
+//   }
+// }
 </script>
 
 <style lang="scss" scoped>
--- src/views/permissionManagement/sourceList/index.vue
+++ src/views/permissionManagement/sourceList/index.vue
@@ -1,334 +1,245 @@
 <template>
-  <div class="wrapper">
-    <div class="search clearfix">
-      <el-button class="fr" type="primary" @click="handleAddRole">添加角色</el-button>
-    </div>
-    <div class="table">
-      <el-table
-        :data="list"
-        row-key="id"
-        border
-        fit
-        highlight-current-row
-        style="width: 100%">
-        <el-table-column align="center" label="排序" width="60">
-          <template slot-scope="scope">
-            <span>{{scope.row.roleSort}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" label="角色名称" width="120">
-          <template slot-scope="scope">
-            <span>{{scope.row.roleName}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" label="角色描述" width="120">
-          <template slot-scope="scope">
-            <span>{{scope.row.describes}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" label="创建人" width="120">
-          <template slot-scope="scope">
-            <span>{{scope.row.createName}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" label="状态" width="80">
-          <template slot-scope="scope">
-            <span>{{scope.row.enabled === '1' ? '启用' : '禁用'}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" label="创建时间" :formatter="getCreateTime">
-        </el-table-column>
-        <el-table-column align="center" label="修改时间" :formatter="getUpdateTime">
-        </el-table-column>
-        <el-table-column align="center" label="操作">
-          <template slot-scope="scope">
-            <div v-if="scope.row.roleName === '超级管理员'">
-              ----
-            </div>
-            <div v-else>
-              <el-button
-                size="mini"
-                type="primary"
-                @click="handleCheck(scope.row.roleId)"
-              >操作</el-button>
-              <el-button
-                size="mini"
-                type="warning"
-                @click="handleAllot(scope.row.roleId)"
-              >分配权限</el-button>
-              <el-button
-                size="mini"
-                type="danger"
-                @click="handleDeleteRole(scope.row.roleId)"
-              >删除</el-button>
-            </div>
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
-    <el-dialog
-      title="提示"
-      :visible.sync="dialogVisible"
-      width="330px">
-      <div>
-        <el-row>
-        <span>角色名称：</span>
-        <el-input
-          style="width: 200px"
-          v-model="roleName"
-          placeholder="请输入角色名称">
-        </el-input>
-        </el-row>
-        <el-row style="margin-top: 20px">
-        <span>是否禁用：</span>
-        <el-select
-          style="width: 200px"
-          v-model="enabled"
-          placeholder="请选择">
-          <el-option
-            v-for="item in enableOptions"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
-          </el-option>
-        </el-select>
-        </el-row>
-      </div>
-      <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="onRoleSubmit">确 定</el-button>
-      </span>
-    </el-dialog>
-    <el-dialog
-      title="分配权限"
-      :visible.sync="dialogRoleVisible"
-      width="300px">
-      <div>
-        <el-tree
-          :data="treeData"
-          ref="tree"
-          show-checkbox
-          node-key="id"
-          :default-checked-keys="defaultTreeData"
-          :props="defaultProps">
-        </el-tree>
-      </div>
-      <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="onAllotubmit">确 定</el-button>
-      </span>
-    </el-dialog>
-    <el-dialog
-      title="提示"
-      :visible.sync="updateWrapper"
-      width="420px">
-      <el-form label-width="100px" :model="adminForm" ref="adminForm" :rules="rules">
-        <el-form-item label="排序：" prop="roleSort">
-          <el-input
-            style="width: 240px"
-            placeholder="请输入账号"
-            v-model="adminForm.roleSort"></el-input>
-        </el-form-item>
-        <el-form-item label="角色名称：" prop="roleName">
-          <el-input
-            style="width: 240px"
-            placeholder="角色名称"
-            v-model="adminForm.roleName"></el-input>
-        </el-form-item>
-        <el-form-item label="角色描述：" prop="describes">
-          <el-input
-            style="width: 240px"
-            placeholder="角色描述"
-            v-model="adminForm.describes"></el-input>
-        </el-form-item>
-        <el-form-item label="是否禁用：" prop="enabled">
-          <el-select
-            style="width: 240px"
-            v-model="adminForm.enabled"
-            placeholder="请选择">
-            <el-option
-              v-for="item in enableOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="onSubmitUpdate">确 定</el-button>
-      </span>
-    </el-dialog>
+  <div class="map-wrapper" v-loading="show">
+    <!-- <el-button type="primary" style="position: absolute; z-index: 999; top: 1vh; left: 2vw;" @click="reset">暂停</el-button> -->
+    <el-button type="primary" size="small" style="position: absolute; z-index: 999; top: 3vh; left: 2vw;" @click="go">重播</el-button>
+    <baidu-map class="map" :center="path[3]" :zoom="15" :style="{width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0, 0.5)'} " :ak="'7vVOlMOKr03PaWX82WajF6m'"
+      :mapClick="false"
+      :scroll-wheel-zoom="false"
+      @click="getPoint"
+      @ready="handler">
+      <bm-marker style="z-index: 9999; position: absolute" v-for="(item, index) in dataTo" :position="{lng: item.lng, lat: item.lat}" :key="index"  @click="handleInfo"  :title="`${item.lng}-${item.lat}`">
+      </bm-marker>
+      <bm-driving :start="{
+        lng: 104.654793,
+        lat: 28.771449
+      }" :end="{
+        lng: 104.705663,
+        lat: 28.802875
+      }" :panel="false" :autoViewport="false"></bm-driving>
+      <bml-lushu
+        @stop="reset"
+        :path="path"
+        :icon="icon"
+        :play="play"
+        :speed="500"
+        :content="content"
+        :autoView="true"
+        :rotation="true">
+      </bml-lushu>
+    </baidu-map>
   </div>
 </template>
 
 <script>
-import moment from 'moment'
+import { BaiduMap, BmlLushu, BmDriving, BmMarker } from 'vue-baidu-map'
+import { setTimeout } from 'timers'
 export default {
-  name: 'Role',
+  components: {
+    BaiduMap,
+    BmlLushu,
+    BmDriving,
+    BmMarker
+  },
   data () {
     return {
-      list: [],
-      dialogVisible: false,
-      dialogRoleVisible: false,
-      updateWrapper: false,
-      roleName: '',
-      enabled: '',
-      allotId: '',
-      adminForm: {
-        roleId: '',
-        roleSort: '',
-        roleName: '',
-        describes: '',
-        enabled: ''
-      },
-      rules: {
-        roleSort: [
-          { required: true, message: '请输入序号', trigger: 'blur' }
-        ],
-        roleName: [
-          { required: true, message: '请输入角色名称', trigger: 'blur' }
-        ],
-        describes: [
-          { required: true, message: '请输入角色描述', trigger: 'blur' }
-        ],
-        enabled: [
-          { required: true, message: '请选择状态', trigger: 'blur' }
-        ]
-      },
-      enableOptions: [
+      address: '',
+      show: true,
+      content: '123',
+      play: true,
+      dataTo: [
         {
-          value: '1',
-          label: '启用'
+          lng: 104.654793,
+          lat: 28.771449,
+          bShow: false,
+          html: `<div>123</div>`,
+          pauseTime: 500
         },
         {
-          value: '0',
-          label: '禁用'
+          lng: 104.653123,
+          lat: 28.7654,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 10,
+          bShow: false
+        },
+        {
+          lng: 104.666257,
+          lat: 28.774314,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.674613,
+          lat: 28.781925,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.684964,
+          lat: 28.791632,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.700523,
+          lat: 28.796219,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.705989,
+          lat: 28.797774,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.705663,
+          lat: 28.802875,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
         }
       ],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      },
-      treeData: [],
-      defaultTreeData: [],
-      total: '',
-      currentPage: 1
+      path: [],
+      kobe: [],
+      icon: {
+        url: 'http://api.map.baidu.com/library/LuShu/1.2/examples/car.png',
+        size: { width: 50, height: 26 },
+        opts: { anchor: { width: 27, height: 13 } }
+      }
     }
   },
-  created () {
-    this.getSysRoleList()
+  mounted () {
+    this.show = true
+    setTimeout(() => {
+      this.show = false
+      this.path = [
+        {
+          lng: 104.654793,
+          lat: 28.771449,
+          bShow: false,
+          html: `<div>123</div>`,
+          pauseTime: 500
+        },
+        {
+          lng: 104.653123,
+          lat: 28.7654,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 10,
+          bShow: false
+        },
+        {
+          lng: 104.666257,
+          lat: 28.774314,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.674613,
+          lat: 28.781925,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.684964,
+          lat: 28.791632,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.700523,
+          lat: 28.796219,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.705989,
+          lat: 28.797774,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        },
+        {
+          lng: 104.705663,
+          lat: 28.802875,
+          value: '123',
+          html: `<div>123</div>`,
+          pauseTime: 5,
+          bShow: false
+        }
+      ]
+    }, 1000)
   },
   methods: {
-    getSysRoleList () {
-      this.$api['role.list']({
-        pageSize: 10,
-        pageNumber: this.currentPage
-      }).then(res => {
-        this.list = res.list
-      })
+    handler ({ BMap, map }) {
+      this.zoom = 18
+      this.geocoder = new BMap.Geocoder()
+      // this.center = this.center
     },
-    onRoleSubmit () {
-      if (this.roleName.length > 0) {
-        this.$api['role.add']({
-          roleName: this.roleName,
-          enabled: this.enabled
-        }).then(res => {
-          this.getSysRoleList()
-          this.$message.success('添加成功!')
-          this.dialogVisible = false
+    reset () {
+      this.play = false
+    },
+    getPoint (e) {
+      if (!this.isErrorPosition) {
+        this.geocoder.getLocation(e.point, res => {
+          this.address = res.address
+          // console.log(this.address)
         })
       } else {
-        this.$message.error('请输入角色名称')
+        this.address = ''
       }
     },
-    handleDeleteRole (id) {
-      this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        this.$api['role.delete']({
-          id: id
-        }).then(res => {
-          this.getSysRoleList({
-            pageSize: 10,
-            pageNumber: this.currentPage
-          })
-          this.$message.success('删除成功！')
-        })
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '已取消删除'
-        })
-      })
+    go () {
+      this.play = true
     },
-    handleAllot (id) {
-      this.dialogRoleVisible = true
-      this.allotId = id
-      // queryPermission('sys/function/function/query', {
-      //   roleId: id
-      // }).then(res => {
-      //   if (res.code === 0) {
-      //     this.treeData = res.data.allTreeMapList
-      //     this.defaultTreeData = res.data.checkNodeIds
-      //   }
+    handleInfo (e) {
+      console.log(event)
+      if (!this.isErrorPosition) {
+        this.geocoder.getLocation(e.point, res => {
+          this.address = res.address
+          console.log(this.address)
+        })
+      } else {
+        this.address = ''
+      }
+      // this.$notify({
+      //   title: item.lng,
+      //   message: item.lat,
+      //   duration: 0
       // })
-    },
-    onAllotubmit () {
-      // const checkedKeys = this.$refs.tree.getCheckedKeys()
-      // updateSysRole('sys/role/function/update', {
-      //   checkedKeys: checkedKeys.join(','),
-      //   sysRoleId: this.allotId
-      // }).then(res => {
-      //   if (res.code === 0) {
-      //     this.dialogRoleVisible = false
-      //     this.$message.success('分配成功！')
-      //   }
-      // })
-    },
-    handleCheck (id) {
-      this.updateWrapper = true
-      this.adminForm.roleId = id
-      this.$api['role.detail']({
-        id
-      }).then(res => {
-        this.adminForm.roleName = res.roleName
-        this.adminForm.describes = res.describes
-        this.adminForm.roleSort = res.roleSort
-        this.adminForm.enabled = res.enabled
-      })
-    },
-    onSubmitUpdate () {
-      this.$api['role.update'](this.adminForm).then(res => {
-        this.getSysRoleList({
-          pageSize: 10,
-          pageNumber: this.currentPage
-        })
-        this.$message.success('修改成功！')
-        this.updateWrapper = false
-      })
-    },
-    handleAddRole () {
-      this.roleName = ''
-      this.dialogVisible = true
-    },
-    getCreateTime (row) {
-      return moment(row.createTime).format('YYYY-MM-DD HH:mm:ss')
-    },
-    getUpdateTime (row) {
-      return moment(row.updateTime).format('YYYY-MM-DD HH:mm:ss')
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.wrapper {
+<style scoped lang="scss">
+.map-wrapper {
   width: 100%;
-  padding: 20px 20px;
-  box-sizing: border-box;
-  .search {
-    margin-bottom: 20px;
+  height: 100%;
+  .map {
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
