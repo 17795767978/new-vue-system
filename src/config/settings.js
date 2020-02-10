546fe3399478e64aadd293c215a48ffbe99d6547
吴洋 <wuyang@wuyangdeMacBook-Pro.local>
2020-02-10 14:12:54 +0800

    提交信息

--- src/config/settings.js
+++ src/config/settings.js
@@ -21,11 +21,12 @@ let id = Math.random().toString(36).substr(3, 30)
 // } else {
 //   baseUrl = 'http://172.16.100.160:8081'
 // }
-export const WSAPI = `ws://117.34.118.30:8010/websocket/mesgshow/${id}`
+export const WSAPI = `ws://121.10.248.110:8010/websocket/mesgshow/${id}`
 export const API_DEFAULT_CONFIG = {
   // mockBaseUrl: 'http://192.168.10.145:8082',
   // mockBaseUrl: 'http://111.62.52.35:8089',
-  mockBaseUrl: 'http://117.34.118.30:8010',
+  // mockBaseUrl: 'http://117.34.118.30:8010',
+  mockBaseUrl: 'http://121.10.248.110:8010',
   // mockBaseUrl: 'http://61.157.184.120:8088',
   // mockBaseUrl: 'http://192.168.10.23:8081',
   // mockBaseUrl: 'http://192.168.10.81:8081',
@@ -34,9 +35,9 @@ export const API_DEFAULT_CONFIG = {
   // mockBaseUrl: 'http://192.168.10.33:8081',
   // 10.233 8089
   // prodBaseUrl: 'http://192.168.10.233:8089', // 测试
-  // prodBaseUrl: 'http://117.34.118.30:8089', // 自测
+  prodBaseUrl: 'http://121.10.248.110:8010', // 肇庆
   // prodBaseUrl: 'http://61.157.184.120:8088', // 宜宾
-  prodBaseUrl: 'http://117.34.118.30:8010', // 自测
+  // prodBaseUrl: 'http://117.34.118.30:8010', // 自测
   // prodBaseUrl: 'http://111.62.52.35:8089', // 邢台
   isMocked: process.env.NODE_ENV !== 'production',
   isDebug: true,
