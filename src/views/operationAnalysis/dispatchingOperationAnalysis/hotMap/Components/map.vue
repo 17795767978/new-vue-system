3d7a717469b11335db4c87de00865cdf194396a0
wuyang <wuyang@cictec.cn>
2020-05-11 18:44:31 +0800

    修改需求

--- src/views/operationAnalysis/dispatchingOperationAnalysis/hotMap/Components/map.vue
+++ src/views/operationAnalysis/dispatchingOperationAnalysis/hotMap/Components/map.vue
@@ -12,7 +12,7 @@
       v-loading="loading"
       element-loading-background="rgba(0, 0, 0, 0)"
     >
-      <bml-heatmap :data="hotdata" :max="2000" :radius="15">
+      <bml-heatmap :data="hotdata" :max="400" :radius="15">
       </bml-heatmap>
     </baidu-map>
  </div>
