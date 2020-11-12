fec4daa73d02486c196bffb1daa0288dc905a556
lisw <shuaiwu123@foxmail.com>
2020-11-12 17:03:14 +0800

    提交 package 文件

--- src/config/settings.js
+++ src/config/settings.js
@@ -21,7 +21,7 @@ let id = Math.random().toString(36).substr(3, 30)
 // } else {
 //   baseUrl = 'http://172.16.100.160:8081'
 // }
-const baseUrl = process.env.NODE_ENV === 'production' ? '117.34.118.30:8288' : '192.168.10.81:8089'
+const baseUrl = process.env.NODE_ENV === 'production' ? '117.34.118.30:8288' : '117.34.118.30:8288'
 export const WSAPI = `ws://${baseUrl}/websocket/mesgshow/${id}`
 export const WSAPISEC = `ws://${baseUrl}/websocket/can/${id}`
 export const WSPOSITION = {
@@ -41,7 +41,7 @@ export const API_DEFAULT_CONFIG = {
   // mockBaseUrl: 'http://117.34.118.30:8288',
   // 192.168.10.33:8081
   // mockBaseUrl: 'http://192.168.10.145:8081'1,
-  mockBaseUrl: 'http://192.168.10.98:8081',
+  mockBaseUrl: 'http://117.34.118.30:8288',
   // 10.233 8089
   prodBaseUrl: 'http://117.34.118.30:8288', // 测试
   // prodBaseUrl: 'http://117.34.118.30:8089', // 自测
